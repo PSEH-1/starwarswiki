3852d81f6673ec546ff660d353d48e2d3e5d5d5f
Sapient_C <sapient_c@sapient.com>
2019-08-05 12:12:10 +0530

    initial commit

---
+++ src/main/java/com/starwarswiki/app/starwarswiki/StarwarswikiApplication.java
@@ -0,0 +1,13 @@
+package com.starwarswiki.app.starwarswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarswikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarswikiApplication.class, args);
+	}
+
+}
